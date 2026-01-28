410ec15ae88fd55da415b50d530103021808b7a0
vpnanonymous69 <vpnanonymous69@gmail.com>
2026-01-28 23:07:23 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+num1=float(input('enter first number: '))
+num2=float(input('enter second number: '))
+print('addition : ', num1+num2 )
+print('subtraction : ', num1-num2 )
+print('multiplication : ', num1*num2 )
+print('division : ', num1/num2 )
---
+++ task2.py
@@ -0,0 +1,3 @@
+fname= input('enter your first name: ')
+lname= input('enter your last name: ')
+print('Hello,', fname, lname,'! Welcome to the pyhton program.')
